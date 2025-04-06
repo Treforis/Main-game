732532b2abcc4c75d147ad1458d96a974a990e7d
Treforis <najibmauthoor@gmail.com>
2025-04-06 11:05:39 -0400

    Character
    Movement/animations

---
+++ Character/luffy__body.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+@onready var animated_sprite = $AnimatedSprite2D
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		animated_sprite.play("Jump")
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if velocity.x != 0:
+		animated_sprite.play("Running")
+		animated_sprite.flip_h = velocity.x > 0
+	else:
+		animated_sprite.play("Idle")
+
+	move_and_slide()
