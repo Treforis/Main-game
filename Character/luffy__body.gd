8b9e3d32fb3bb80d08bd7b66376a223c8bc9c9a5
Treforis <najibmauthoor@gmail.com>
2025-04-07 18:13:08 -0400

    Punch animation
    Punching animation with set timer

--- Character/luffy__body.gd
+++ Character/luffy__body.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 @onready var animated_sprite = $AnimatedSprite2D
-
+@onready var Punch_Timer = $Punchtimer
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
@@ -16,14 +16,27 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if Input.is_action_just_pressed("punch") and Punch_Timer.is_stopped():
+		animated_sprite.play("Punch")
+		Punch_Timer.start()
+		
+	if Punch_Timer.is_stopped():
+		if not is_on_floor():
+			animated_sprite.play("Jump")
+		elif velocity.x != 0:
+			animated_sprite.play("Running")
+			animated_sprite.flip_h = velocity.x > 0
+		else:
+			animated_sprite.play("Idle")
 
-	# Animation logic (jump takes priority)
-	if not is_on_floor():
-		animated_sprite.play("Jump")
-	elif velocity.x != 0:
-		animated_sprite.play("Running")
-		animated_sprite.flip_h = velocity.x > 0
-	else:
-		animated_sprite.play("Idle")
 
 	move_and_slide()
+	
+func _on_PunchTimer_timeout():
+	animated_sprite.stop()
+
+func punch():
+	$PunchHitbox.monitoring = true
+	$PunchHitbox.visible = true  
+	$PunchHitbox/Timer.start()
